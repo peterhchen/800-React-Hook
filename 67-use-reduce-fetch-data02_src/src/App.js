a1ace91c6c49025a38ce48c00b1ca38d6c4c1be5
peterhchen <peter.hp.chen@gmail.com>
2020-04-17 17:37:45 -0700

    Add files via upload

---
+++ 67-use-reduce-fetch-data02_src/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+// import DataFetching from './components/DataFetching'
+import DataFetchingTwo from './components/DataFetchingTwo'
+
+function App() {
+  return (
+    <div className="App">
+      {/* <DataFetching  /> */}
+      <DataFetchingTwo />
+    </div>
+  );
+}
+
+export default App;
