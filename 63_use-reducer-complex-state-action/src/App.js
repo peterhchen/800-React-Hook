c516db50160df6cf046a08ee0cb2f530b2e6d7a1
peterhchen <peter.hp.chen@gmail.com>
2020-04-17 17:33:51 -0700

    Add files via upload

---
+++ 63_use-reducer-complex-state-action/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+// import CounterOne from './components/CounterOne'
+import CounterTwo from './components/CounterTwo'
+function App() {
+  return (
+    <div className="App">
+      <CounterTwo />
+      {/* <CounterOne /> */}
+    </div>
+  );
+}
+export default App;
