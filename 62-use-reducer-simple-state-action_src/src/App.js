6fa7bca97c6ad0cf2a666a08a7ec08ad37449620
peterhchen <peter.hp.chen@gmail.com>
2020-04-17 17:32:15 -0700

    Add files via upload

---
+++ 62-use-reducer-simple-state-action_src/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import CounterOne from './components/CounterOne'
+
+function App() {
+  return (
+    <div className="App">
+      <CounterOne />
+    </div>
+  );
+}
+export default App;
