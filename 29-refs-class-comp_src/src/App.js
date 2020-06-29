a800a659a9988d74d6a8aa9c834c05df7a99dd61
peterhchen <peter.hp.chen@gmail.com>
2020-06-29 11:49:55 -0700

    Add files via upload
    Pass refs from parent to child.
    Parent calls child method for focus.

---
+++ 29-refs-class-comp_src/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import FocusInputClass  from './components/FocusInputClass'
+
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+        <FocusInputClass />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ 29-refs-class-comp_src/src/components/RefsDemo.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+class RefsDemo extends Component {
+    constructor(props) {
+      super(props)
+      // First Step: Create the inputRef.
+      this.inputRef = React.createRef ();
+    }
+    componentDidMount () {
+        // Third Step: 1) Set focus. 
+        this.inputRef.current.focus()
+        console.log (this.inputRef)
+    }
+    clickHandler = () => {
+        alert (this.inputRef.current.value)
+    }
+  render() {
+    return (
+      <div>
+        {/* <input type="text"/>  */}
+        {/* Second Step: Attach the reference to the input element. */}
+        <input type="text" ref={ this.inputRef } /> 
+        {/* Third Step: 2) Add element handler for reference. */}
+        <button onClick={ this.clickHandler}>Click </button>
+      </div>
+    )
+  }
+}
+
+export default RefsDemo
